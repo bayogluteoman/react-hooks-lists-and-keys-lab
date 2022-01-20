6f5d67f3e778c6b6b3d0d9e644452d76f397541d
bayogluteoman <bayogluteoman@gmail.com>
2022-01-21 02:37:17 +0300

    complete lists nad keys lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const techList = Object.entries(technologies).map(([key, value]) => {
+    return (
+      <span key={key}> {value.toString()} </span>
+    );
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      <div className="technologies" >
+        {techList}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,23 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {
+          projects.map((e) => {
+            return (
+              <ProjectItem
+                key={e.id.toString()}
+                name={e.name}
+                about={e.about}
+                technologies={e.technologies} />
+            )
+          }
+          )
+        }
+      </div>
     </div>
   );
 }
